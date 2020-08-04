01a0489c503097db28c1b6fdeb9d95aa6f1e542a
Jeongminchang <49319275+min413@users.noreply.github.com>
2020-08-04 11:30:47 +0900

    express

---
+++ main.js
@@ -0,0 +1,42 @@
+var express = require('express')
+var app = express()
+var fs = require('fs');
+var qs = require('querystring');
+var bodyParser = require('body-parser');
+var compression = require('compression');
+var helmet = require('helmet');
+app.use(helmet());
+
+var indexRouter = require('./routes/index.js');
+var topicRouter = require('./routes/topic.js');
+
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(compression());
+app.get('*', function(request, response, next){//get 방식을 하는 모든 요청
+  fs.readdir('./data', function(error, filelist){
+    request.list = filelist;
+    next();
+  });
+});
+app.use('/', indexRouter);
+app.use('/topic',topicRouter);
+
+
+
+
+
+
+app.use(function(req, res, next) {
+  res.status(404).send('Sorry cant find that!');
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+});
+
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!')
+});
